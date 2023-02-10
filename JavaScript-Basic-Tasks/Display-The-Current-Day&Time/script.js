7aa390cb30edd6030913f0c77ea8fa5b620a7e00
Sam Lam <106876072+Lam-Big-Water@users.noreply.github.com>
2023-02-10 14:30:19 +0800

    basic 01

---
+++ JavaScript-Basic-Tasks/Display-The-Current-Day&Time/script.js
@@ -0,0 +1,54 @@
+// The Sample Solution
+const today = new Date();
+const day = today.getDay();
+const daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+console.log(`Today is : ${daylist[day]}.`);
+
+let hour = today.getHours();
+const minute = today.getMinutes();
+const second = today.getSeconds();
+
+let prepand = (hour >= 12) ? 'PM' : 'AM';
+hour = (hour >= 12) ? hour - 12 : hour;
+if (hour === 0 && prepand === 'PM') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = 'Nood';
+    } else {
+        hour = 12;
+        prepand = 'PM';
+    }
+}
+
+if (hour === 0 && prepand === 'AM') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = 'Midnight';
+    } else {
+        hour = 12;
+        prepand = 'AM';
+    }
+}
+
+console.log(`Current Time : ${hour}${prepand} : ${minute} : ${second}`);
+
+// The Latest Solution
+
+const currentDayTime = () => {
+    let today = new Date();
+    let day = today.getDay();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+
+    let daylist = ['Sunday', 'Monday', 'Tuesaday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let t = '';
+
+    t = (hour >= 12) ? t = 'PM' : t = 'AM';
+    minute = (minute < 10) ? minute = '0' + minute : minute;
+    second = (second < 10) ? second = '0' + second : second;
+
+    return `Today is : ${daylist[day]} Current time is ${hour}${t}:${minute}:${second}`;
+}
+console.log(currentDayTime());
